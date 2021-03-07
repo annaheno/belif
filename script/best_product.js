5b2b9f481eb5b2f09f1952f3fad648b4f48d61c6
annaheno <71163939+annaheno@users.noreply.github.com>
2021-03-07 23:57:17 +0900

    Create best_product.js

---
+++ script/best_product.js
@@ -0,0 +1,51 @@
+$(function () {
+	var leftValue = 0;
+	var maxLeft = 0;
+
+	setInterval(function () {
+		maxLeft = $(".best_product > .best > ul").outerWidth() - $("#bestlist").outerWidth();
+
+		if (leftValue <= -maxLeft) {
+			leftValue = -maxLeft;
+			$(".best_product > .best > ul").css({
+				left: leftValue
+			});
+		} else {
+			$("button.btn_next").css({
+				background: "url(/images/btn_next.png"
+			});
+		}
+	}, 20);
+
+	$("button.btn_next").click(function () {
+		$("button.btn_prev").css({
+			background: "url(/images/btn_prev.png)"
+		});
+		leftValue -= 220;
+		if (leftValue <= -maxLeft) {
+			leftValue = -maxLeft;
+			$("button.btn_next").css({
+				background: "url(images/btn_next.png)"
+			});
+		}
+		$(".best_product > .best > ul").css({
+			left: leftValue
+		});
+	});
+
+	$("button.btn_prev").click(function () {
+		$("button.btn_next").css({
+			background: "url(images/btn_next.png)"
+		});
+		leftValue += 220;
+		if (leftValue >= 0) {
+			leftValue = 0;
+			$("button.btn_prev").css({
+				background: "url(images/btn_prev.png)"
+			});
+		}
+		$(".best_product > .best > ul").css({
+			left: leftValue
+		});
+	});
+});
