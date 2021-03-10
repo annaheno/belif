ba70fa021130fdc84aadb1db5bec65fd75bfd861
annaheno <71163939+annaheno@users.noreply.github.com>
2021-03-10 15:20:56 +0900

    Update best_product.js

--- script/best_product.js
+++ script/best_product.js
@@ -12,20 +12,20 @@ $(function () {
 			});
 		} else {
 			$("button.btn_next").css({
-				background: "url(images/btn_next_a.png"
+				background: "url(/images/btn_next_a.png"
 			});
 		}
 	}, 20);
 
 	$("button.btn_next").click(function () {
 		$("button.btn_prev").css({
-			background: "url(images/btn_prev_n.png)"
+			background: "url(/images/btn_prev_n.png)"
 		});
 		leftValue -= 220;
 		if (leftValue <= -maxLeft) {
 			leftValue = -maxLeft;
 			$("button.btn_next").css({
-				background: "url(images/btn_next_a.png)"
+				background: "url(/images/btn_next_a.png)"
 			});
 		}
 		$(".best_product > .best > ul").css({
@@ -35,13 +35,13 @@ $(function () {
 
 	$("button.btn_prev").click(function () {
 		$("button.btn_next").css({
-			background: "url(images/btn_next_a.png)"
+			background: "url(/images/btn_next_a.png)"
 		});
 		leftValue += 220;
 		if (leftValue >= 0) {
 			leftValue = 0;
 			$("button.btn_prev").css({
-				background: "url(images/btn_prev_n.png)"
+				background: "url(/images/btn_prev_n.png)"
 			});
 		}
 		$(".best_product > .best > ul").css({
